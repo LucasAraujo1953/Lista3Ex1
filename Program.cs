b57fb7d8230ec2595d229c792f9e448853b03dd0
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-05-09 23:32:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double v;
+            double total;
+            int k;
+
+            total = 0;
+            k = 1;
+            do
+            {
+                Console.Write("Digite o valor: ");
+                v = double.Parse(Console.ReadLine());
+                total = total + v;
+                k = k + 1;
+            }
+            while (v > 0);
+
+        }
+    }
+}
